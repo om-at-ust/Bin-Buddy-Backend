e9ecde7d0d0082f6a1b334687d750cd3e45f2170
om-at-ust <omdubey737683r@gmail.com>
2024-11-11 10:06:40 +0530

    fix authentication service

--- binService/src/main/java/com/CapstoneProject/Bin/binService/controller/BinController.java
+++ binService/src/main/java/com/CapstoneProject/Bin/binService/controller/BinController.java
@@ -3,6 +3,9 @@ package com.CapstoneProject.Bin.binService.controller;
 import com.CapstoneProject.Bin.binService.entity.Bin;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
